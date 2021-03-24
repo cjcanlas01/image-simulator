a7124da4cdfffe5a19ff872296f77a219f58d0ce
cjcanlas01 <cjcanlas78@gmail.com>
2021-03-24 14:11:28 +0800

    feat: add default message on index and check if design file exists

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const sharp = require("sharp");
 const path = require("path");
+const fs = require("fs");
 const server = express();
 
 /**
@@ -20,24 +21,39 @@ const generateImage = async (file) => {
   const fileArgs = file.split(".");
   const template = "template/template.jpg";
   const design = `design/${fileArgs[0]}.png`;
+
+  if (!fs.existsSync(withRootPath(design)))
+    return "Design file does not exists!";
+
   const output = `output/${file}`;
   const details = await sharp(template)
-    .composite([{ input: design, gravity: "southeast" }])
+    .composite([{ input: design }])
     .toFile(output);
 
-  return {
-    details,
-    output,
-  };
+  return details;
 };
 
-server.get("/masks/:file", async (req, res) => {
-  const file = req.params.file;
-  const image = await generateImage(file);
+server.get("/", async (req, res) => {
+  res.send("<h1>Hello from Simulation File!</h1>");
+});
 
-  if (image.details) {
-    res.sendFile(withRootPath(image.output));
+server.get("/autogen/:file", async (req, res) => {
+  const file = req.params.file;
+  const outputPath = withRootPath(`output/${file}`);
+
+  if (!fs.existsSync(outputPath)) {
+    const output = await generateImage(file);
+    /**
+     * Check if file is generated or not
+     * returns message if not generated
+     */
+    if (typeof output == "string") {
+      res.send(output);
+      return;
+    }
   }
+
+  res.sendFile(outputPath);
 });
 
 server.listen(process.env.PORT, () => {
